4b53715a37255c38039b8f803f810b3d1bcfe63a
გიორგი ენუქიძე <56786173+giorgi-333@users.noreply.github.com>
2025-03-10 17:36:55 +0400

    #2

---
+++ js/2/main.js
@@ -0,0 +1,39 @@
+
+
+
+// console.log(y)
+
+// var x = 4
+// let dark_mode = true
+
+
+// console.log(5);
+
+// y = 25
+
+// console.log(25);
+
+// const pi = 3.14
+
+// let saxeli
+// let saxeli12
+// let Saxeli
+// let saxeliGvari
+// let saxeli_gvari
+// let _saxeli
+// let $saxeli
+
+// function printTest() {
+//     console.log('test');
+// }
+
+
+function shetyobineba() {
+    // let name = 'nika'
+    // alert("რაიმე ტექსტი")
+    // let result = document.getElementById("result-1")
+    let result = document.querySelector("#result-1.ab")
+    console.log(result.innerHTML);
+    result.innerHTML = "ახალი ტექსტი";
+}
+
