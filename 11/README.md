34d70a5987e2ea0657e96315a8c311629fe62756
giorgi-333 <giorgienuqidze2000@gmail.com>
2025-04-03 17:26:32 +0400

    Add 11

---
+++ 11/README.md
@@ -0,0 +1,35 @@
+
+# დავალება 1
+
+
+**1.1** დაბეჭდეთ სია HTML ფაილში და გაიფილტროს input-ების დახმარებით
+- ასაკი უნდა იყოს ჩაწერილ პირველ ციფრზე მეტი და მეორეზე ნაკლები
+
+<!-- <input style="width: 35px"/>-დან <input style="width: 35px"/>-მდე -->
+
+![თუ სურათი არ ჩანს მითხარით](fil.png)
+
+```js
+const persons = [
+  {
+    name: "nika",
+    surname: "shavdia",
+    age: 17
+  },
+  {
+    name: "giga",
+    surname: "metreveli",
+    age: 18
+  },
+  {
+    name: "lasha",
+    surname: "gomarteli",
+    age: 25
+  },
+  {
+    name: "vano",
+    surname: "tabagari",
+    age: 28
+  }
+]
+```
---
+++ 11/main.js
@@ -0,0 +1,67 @@
+
+
+// let value  = ""
+
+// let x = value || 10
+// //  ეს არის მოკლე ჩანაწერი, ქვემოთ მოცემული if else-ის
+// if (value) {
+//     x = value
+// } else {
+//     x = 10
+// }
+
+
+
+// if-ში false-ს აბრუნებს null, undifined, ""(ცარიელი სთრიგნი)
+
+// x.trim() შლის დაშორებებს
+
+let numbers = [34, 54, 23, "qutaisi", 24, 244, 2333, "tbilisi"]
+let objsList = [
+    {
+        name: "nika",
+        surname: "shavdia",
+        age: 17
+    },
+    {
+        name: "giga",
+        surname: "metreveli",
+        age: 28
+    },
+    {
+        name: "sxva",
+        surname: "metreveli",
+        age: 40
+    }
+]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// for (let i in numbers) {
+//     console.log(numbers[i]);
+// }
+
+// for (let item of numbers) {
+//     console.log(item);
+// }
+
+// objsList.forEach( (item, i) => {
+//     console.log(item, item.name, i);
+// })
+
+
+// filter() აბრუნებს ახალ გაფილტრულ მასივს
+
+console.log(objsList);
+
+let newList = objsList.filter(item => {
+    return item.age > 18
+})
+
+console.log(objsList);
+
+console.log("ახალი სია: ", newList);
+
+
